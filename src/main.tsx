c1f404bafe0f69f9a02e1113d72a18102e850e1a
sikkzz <sling0623@gmail.com>
2025-01-19 23:53:24 +0900

    chore: tanstack query@5 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import App from "@/App";
+
+import ReactQueryClientProvider from "@/components/provider/ReactQueryClientProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ReactQueryClientProvider>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ReactQueryClientProvider>
   </StrictMode>,
 );
