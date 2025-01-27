ae74eedd6266b9a7e754e884131241736a193c3f
sikkzz <sling0623@gmail.com>
2025-01-28 00:36:16 +0900

    feat: module scss 적용 설정

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import classNames from "classnames";
+
+import styles from "@/components/Button.module.scss";
+
+const Button = () => {
+  return <button className={classNames(styles.ButtonTest)}>Button</button>;
+};
+
+export default Button;
