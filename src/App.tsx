8e9fdf23fd87e895ef4d10fe134aa15c099192d4
sikkzz <sling0623@gmail.com>
2025-01-28 17:42:36 +0900

    feat: icon 전체 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import Button from "@/components/Button";
+import CameraIcon from "@/assets/svg/ic-camera.svg?react";
+import CloseIcon from "@/assets/svg/ic-close.svg?react";
+import GalleryIcon from "@/assets/svg/ic-gallery.svg?react";
+import LeftArrowIcon from "@/assets/svg/ic-left-arrow.svg?react";
+import PasteIcon from "@/assets/svg/ic-paste.svg?react";
+import PlusIcon from "@/assets/svg/ic-plus.svg?react";
 
 const App = () => {
-  return <Button />;
+  return (
+    <div
+      style={{
+        width: "300px",
+        height: "300px",
+        backgroundColor: "gray",
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+      }}
+    >
+      <GalleryIcon />
+      <CameraIcon />
+      <CloseIcon />
+      <LeftArrowIcon />
+      <PasteIcon />
+      <PlusIcon />
+      <img src="/src/assets/img/img-logo.png" style={{ width: "52px", height: "28px" }} />
+    </div>
+  );
 };
 
 export default App;
