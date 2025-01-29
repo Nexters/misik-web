0cdeccd77df80b2720566ff5916658ef69cf80e6
sikkzz <sling0623@gmail.com>
2025-01-30 00:26:46 +0900

    feat: base button 컴포넌트 추가, button, icon button 컴포넌트 수정

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import classNames from "classnames";
 
+import BaseButton from "@/components/ui/BaseButton/BaseButton";
 import styles from "@/components/ui/Button/Button.module.scss";
 import type { ButtonProps } from "@/components/ui/Button/Button.types";
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant = "primary", text, ...props }, ref) => {
+  (
+    { as = BaseButton, className, variant = "primary", text, disabled = false, onClick, ...props },
+    ref,
+  ) => {
+    const Comp = as as typeof BaseButton;
+
+    const handleClick = useCallback(
+      (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (!disabled) {
+          onClick?.(e);
+        }
+      },
+      [onClick, disabled],
+    );
+
     return (
-      <button
-        className={classNames(styles.Button, styles[`style-${variant}`], className)}
+      <Comp
         ref={ref}
+        className={classNames(styles.Button, styles[`style-${variant}`], className)}
+        disabled={disabled}
+        onClick={handleClick}
         {...props}
       >
         {text}
-      </button>
+      </Comp>
     );
   },
 );
