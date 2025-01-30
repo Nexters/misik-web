e578dad4c2536936b1accbd4d19c83597e4a18bb
sikkzz <sling0623@gmail.com>
2025-01-30 15:32:48 +0900

    feat: 텍스트 컴포넌트 틀 제작

---
+++ src/components/ui/Text/Text.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import classNames from "classnames";
+
+import styles from "@/components/ui/Text/Text.module.scss";
+import type { TextProps } from "@/components/ui/Text/Text.types";
+
+const Text = React.forwardRef<HTMLElement, TextProps>(
+  (
+    {
+      as: Comp = "span",
+      children,
+      size = "default",
+      className,
+      weight = "regular",
+      align,
+      truncated,
+      ...props
+    },
+    ref,
+  ) => {
+    const isMultiLineTruncated = typeof truncated === "number" && truncated >= 1;
+
+    return (
+      <Comp
+        ref={ref}
+        className={classNames(
+          styles.Text,
+          styles[`size-${size}`],
+          styles[`weight-${weight}`],
+          align && styles[`align-${align}`],
+          truncated === true
+            ? styles.truncated
+            : isMultiLineTruncated && styles["multi-line-truncated"],
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </Comp>
+    );
+  },
+);
+
+export default Text;
