501da0491870428a2fe797d4c7cfb6132a82d35b
sikkzz <sling0623@gmail.com>
2025-01-31 02:12:03 +0900

    refactor: input 컴포넌트 variant 스타일 추가

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -5,25 +5,31 @@ import classNames from "classnames";
 import styles from "@/components/ui/Input/Input.module.scss";
 import type { InputProps } from "@/components/ui/Input/Input.types";
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-  const [isFocused, setIsFocused] = useState(false);
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, variant = "primary", ...props }, ref) => {
+    const [isFocused, setIsFocused] = useState(false);
 
-  const handleFocus = () => setIsFocused(true);
-  const handleBlur = () => setIsFocused(false);
+    const handleFocus = () => setIsFocused(true);
+    const handleBlur = () => setIsFocused(false);
 
-  return (
-    <div className={classNames(styles.InputWrapper, { [styles.Focused]: isFocused })}>
-      <input
-        type={type}
-        ref={ref}
-        className={classNames(styles.Input, className)}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        {...props}
-      />
-      <button>수정</button>
-    </div>
-  );
-});
+    return (
+      <div
+        className={classNames(styles.InputWrapper, styles[`style-${variant}`], {
+          [styles.Focused]: isFocused,
+        })}
+      >
+        <input
+          type={type}
+          ref={ref}
+          className={classNames(styles.Input, className)}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          {...props}
+        />
+        {variant === "primary" && <button>수정</button>}
+      </div>
+    );
+  },
+);
 
 export default Input;
