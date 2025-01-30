d0b938124a011213a0f94ccdd2d138c2dcc88ed5
sikkzz <sling0623@gmail.com>
2025-01-30 18:33:40 +0900

    feat: 홈 화면 타이틀, 이미지 로고 추가

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,21 @@
 import styles from "@/components/Home/Home.module.scss";
 import IconButton from "@/components/ui/IconButton/IconButton";
+import Text from "@/components/ui/Text/Text";
 
 const Home = () => {
   return (
     <div className={styles.Home}>
+      <div className={styles.HomeTitle}>
+        <Text size="xl" color="gradient" weight="bold" align="center" as="h1">
+          {`영수증으로\nAI 음식 리뷰 남겨요`}
+        </Text>
+        <Text color="primary" weight="medium" align="center">
+          손쉬운 음식 리뷰 작성
+        </Text>
+      </div>
+      <div className={styles.HomeImage}>
+        <img src="/src/assets/img/img-graphic-logo.png" alt="mainLogo" />
+      </div>
       <div className={styles.HomeBottom}>
         <IconButton text="갤러리" iconName="gallery" />
         <IconButton text="카메라" iconName="camera" />
