b4ca21a1728b396408bb6bb8372e91bf32ce190d
sikkzz <sling0623@gmail.com>
2025-01-31 01:53:59 +0900

    refactor: input 컴포넌트 수정

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react";
+import { useState, forwardRef } from "react";
 
 import classNames from "classnames";
 
@@ -6,9 +6,21 @@ import styles from "@/components/ui/Input/Input.module.scss";
 import type { InputProps } from "@/components/ui/Input/Input.types";
 
 const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
   return (
-    <div className={styles.InputWrapper}>
-      <input type={type} ref={ref} className={classNames(styles.Input, className)} {...props} />
+    <div className={classNames(styles.InputWrapper, { [styles.Focused]: isFocused })}>
+      <input
+        type={type}
+        ref={ref}
+        className={classNames(styles.Input, className)}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        {...props}
+      />
       <button>수정</button>
     </div>
   );
