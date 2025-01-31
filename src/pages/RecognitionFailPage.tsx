d6e972b048fc7943b1d33a56428e4c4f001b7f6b
sikkzz <sling0623@gmail.com>
2025-01-31 15:40:09 +0900

    feat: 영수증 인식 실패 ui 제작

---
+++ src/pages/RecognitionFailPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/common/Navbar";
+import RecognitionFail from "@/components/RecognitionFail/RecognitionFail";
+
+const RecognitionFailPage = () => {
+  return (
+    <>
+      <Navbar />
+      <RecognitionFail />
+    </>
+  );
+};
+
+export default RecognitionFailPage;
