97f5f6cd2920f49736bb4a299e181df1961e8f56
lgrin-byte <lgrin02@naver.com>
2025-02-04 01:47:02 +0900

    feat: 네비바 적용

--- src/pages/SelectStylePage.tsx
+++ src/pages/SelectStylePage.tsx
@@ -1,10 +1,15 @@
-import Navbar from "@/components/common/Navbar";
 import SelectStyle from "@/components/SelectStyle/SelectStyle";
+import Icon from "@/components/ui/Icon/Icon";
+import NavbarV2 from "@/components/ui/NavbarV2/NavbarV2";
 
 const SelectStylePage = () => {
   return (
     <>
-      <Navbar />
+      <NavbarV2>
+        <NavbarV2.LeftButton>
+          <Icon name="leftArrow" />
+        </NavbarV2.LeftButton>
+      </NavbarV2>
       <SelectStyle />
     </>
   );
