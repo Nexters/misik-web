dc1e0bdd84e6bd15222d1f5268de926819251eb8
sikkzz <sling0623@gmail.com>
2025-02-11 22:49:03 +0900

    refactor: home 화면 디자인 수정

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -29,17 +29,20 @@ const Home = () => {
 
   return (
     <div className={styles.Home}>
-      <div className={styles.HomeTitle}>
-        <Text variant="titleLg" color="gradient" align="center" as="h1">
-          {`영수증으로\nAI 음식 리뷰 남겨요`}
-        </Text>
-        <Text variant="bodyLg" color="secondary" align="center">
-          손쉬운 음식 리뷰 작성
-        </Text>
+      <div className={styles.HomeTop}>
+        <div className={styles.HomeTitle}>
+          <Text variant="titleLg" color="gradient" align="center" as="h1">
+            {`영수증으로\nAI 음식 리뷰 남겨요`}
+          </Text>
+          <Text variant="bodyLg" color="secondary" align="center">
+            손쉬운 음식 리뷰 작성
+          </Text>
+        </div>
+        <div className={styles.HomeImage}>
+          <img src="/assets/img/img-graphic-logo.png" alt="mainLogo" />
+        </div>
       </div>
-      <div className={styles.HomeImage}>
-        <img src="/assets/img/img-graphic-logo.png" alt="mainLogo" />
-      </div>
+
       <div className={styles.HomeBottom}>
         <IconButton
           text="갤러리"
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,22 +2,14 @@ import Navbar from "@/components/common/Navbar/Navbar";
 import Home from "@/components/Home/Home";
 import { AppBridgeMessageType } from "@/components/provider/AppBridgeProvider/AppBridgeMessage.types";
 import { useAppBridge } from "@/components/provider/AppBridgeProvider/AppBridgeProvider";
-import Icon from "@/components/ui/Icon/Icon";
 import Text from "@/components/ui/Text/Text";
-
-import { useRoute } from "@/hooks/common/useRoute";
 
 const HomePage = () => {
   const { send } = useAppBridge();
 
-  const { navigateToHome } = useRoute();
-
   return (
     <>
       <Navbar>
-        <Navbar.LeftButton onClick={navigateToHome}>
-          <Icon name="logo" />
-        </Navbar.LeftButton>
         <Navbar.RightButton onClick={() => send({ type: AppBridgeMessageType.SHARE, payload: "" })}>
           <Text variant="bodySm" color="secondary">
             앱 공유하기
