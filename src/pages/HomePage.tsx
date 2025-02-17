1ee914c040717c2dc57821e3d1fb0dbdb3b40f5a
sikkzz <sling0623@gmail.com>
2025-02-18 02:14:36 +0900

    refactor: share 텍스트 추가, 핸들러 파라미터 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,20 @@ import { AppBridgeMessageType } from "@/components/provider/AppBridgeProvider/Ap
 import { useAppBridge } from "@/components/provider/AppBridgeProvider/AppBridgeProvider";
 import Text from "@/components/ui/Text/Text";
 
+const SHARE_TEXT =
+  "영수증을 촬영하면 AI가 자동으로 맛집 리뷰를 생성! 🍽️✨ 간편하게 추억을 남기고, 나만의 미식 기록을 완성하세요. 미식 경험을 더욱 스마트하게, 미식 MISIK!";
+
 const HomePage = () => {
   const { send } = useAppBridge();
 
   return (
     <>
       <Navbar>
-        <Navbar.RightButton onClick={() => send({ type: AppBridgeMessageType.SHARE, payload: "" })}>
+        <Navbar.RightButton
+          onClick={() =>
+            send({ type: AppBridgeMessageType.SHARE, payload: { shareText: SHARE_TEXT } })
+          }
+        >
           <Text variant="bodySm" color="secondary">
             앱 공유하기
           </Text>
