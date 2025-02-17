c332091d15a065cdf821bbe243abd660264b4fcd
sikkzz <sling0623@gmail.com>
2025-02-18 02:09:43 +0900

    refactor: 미사용 코드 삭제

--- src/components/common/SafeAreaWrapper/SafeAreaWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "@/components/common/SafeAreaWrapper/SafeAreaWrapper.module.scss";
-
-const SafeAreaWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div className={styles.SafeArea}>{children}</div>;
-};
-
-export default SafeAreaWrapper;
