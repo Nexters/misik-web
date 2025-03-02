a28e89060f240a1d702279e79c37a91586519c37
sikkzz <sling0623@gmail.com>
2025-03-02 15:41:40 +0900

    refactor: 채널톡 버튼 기본 설정 값 변경

--- src/hooks/common/useChannelTalk.ts
+++ src/hooks/common/useChannelTalk.ts
@@ -3,11 +3,18 @@ import { useEffect } from "react";
 import ChannelService from "@/components/ChannelTalk/channelTalk";
 
 export const useChannelTalk = () => {
+  const openChannelTalk = () => {
+    ChannelService.showMessenger();
+  };
+
   useEffect(() => {
     ChannelService.loadScript();
 
     ChannelService.boot({
       pluginKey: import.meta.env.VITE_CHANNEL_TALK_KEY || "",
+      hideChannelButtonOnBoot: true,
     });
   }, []);
+
+  return { openChannelTalk };
 };
